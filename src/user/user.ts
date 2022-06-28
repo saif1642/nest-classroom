9dbaae4781a9d286b8013b1618612b7ca0117eed
saif1642 <islumsaiful@gmail.com>
2022-06-28 20:01:52 +0600

    user module added

---
+++ src/user/user.ts
@@ -0,0 +1,31 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+export enum UserType {
+  TEACHER = 'teacher',
+  STUDENT = 'student',
+  ADMIN = 'admin',
+}
+
+@Entity('user')
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  first_name: string;
+
+  @Column()
+  last_name: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column({
+    type: 'enum',
+    enum: UserType,
+  })
+  user_type: string;
+}
